7c498258846ddcc25e7a1afd0779d1561a97d76b
Lailatul <mufidalailatul10@gmail.com>
2022-05-18 22:09:52 +0700

    final

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,7 +5,7 @@ import 'package:firebase/pages/first_screen.dart';
 class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
-  }
+}
 
 class _LoginPageState extends State<LoginPage> {
   @override
@@ -20,52 +20,110 @@ class _LoginPageState extends State<LoginPage> {
             children: <Widget>[
               FlutterLogo(size: 150),
               SizedBox(height: 50),
-              _signInButton(),
-              ],
-            ),
-          ),
-        ),
-      );
-    }
-    Widget _signInButton() {
-      return OutlineButton(
-        splashColor: Colors.grey,
-        onPressed: () {
-          signInWithGoogle().then((result) {
-            if (result != null) {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) {
-                    return FirstScreen();
-                    },
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+                child: TextFormField(
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                    hintText: "masukan email",
+                    labelText: "Email",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
                   ),
-                );
-              }
-            });
-          },
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-          highlightElevation: 0,
-          borderSide: BorderSide(color: Colors.grey),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
-                Padding(
-                  padding: const EdgeInsets.only(left: 10),
-                  child: Text(
-                    'Sign in with Google',
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.grey,
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+                child: TextFormField(
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                    hintText: "masukan password",
+                    labelText: "Password",
+                    icon: Icon(Icons.people),
+                    border: OutlineInputBorder(
+                        borderRadius: new BorderRadius.circular(5.0)),
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 15),
+                  width: 250,
+                  height: 55,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 69, 165, 233),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
                       ),
                     ),
-                  )
-                ],
+                    onPressed: () {},
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Color(0xffffffff),
+                      ),
+                    ),
+                  ),
+                ),
+                 Container(
+                margin: const EdgeInsets.only(top: 10, bottom: 15),
+                child: const Text(
+                  "or",
+                  style: TextStyle(fontSize: 20),
+                )
+                ),
+              _signInButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _signInButton() {
+    return OutlineButton(
+      splashColor: Colors.grey,
+      onPressed: () {
+        signInWithGoogle().then((result) {
+          if (result != null) {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return FirstScreen();
+                },
               ),
-            ),
-          );
-        }
-      }
+            );
+          }
+        });
+      },
+      
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      highlightElevation: 0,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+        
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(image: AssetImage("assets/google_logo.png"), height: 35.0),
+            Padding(
+              padding: const EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
